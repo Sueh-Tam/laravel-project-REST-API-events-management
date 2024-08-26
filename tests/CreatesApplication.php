6a390e3b2d37ace99783ec7bc7a702de94c40b93
Matheus Vidal Nunes <matheusvidal2012@gmail.com>
2024-08-26 20:16:37 -0300

    trsete

--- tests/CreatesApplication.php
+++ tests/CreatesApplication.php
@@ -18,7 +18,7 @@ trait CreatesApplication
 
         $app->make(Kernel::class)->bootstrap();
         //return $this->assertFalse(true, 'This test fails intentionally.');
-        $this->assertFalse(false, 'FALHOU COM SUCESSO');
+        $this->asserTrue(false, 'FALHOU COM SUCESSO');
         
         return $app;
     }
